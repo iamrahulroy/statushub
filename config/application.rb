973a3927e8d6dab6e1f78e4c6485d548f2f362c1
Rahul Roy <techniciablog@gmail.com>
2015-11-22 12:18:02 +0530

    Add user devise model

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,14 @@ module Statushub
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Configure generator
+    config.generators do |g|
+      g.test_framework false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.assets false
+    end
   end
 end
